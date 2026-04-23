8daf5a5ddcda68e1f64ce5d0e35fb2b201824b56
EudesHDN <hodonoueudeshdn@gmail.com>
2026-04-23 01:22:39 -0700

    env saved

--- frontApp/src/pages/CandidaturesPage.jsx
+++ frontApp/src/pages/CandidaturesPage.jsx
@@ -26,7 +26,7 @@ function CandidaturesPage() {
             
         
             <header className="flex flex-wrap justify-between sm:justify-center items-center justify-between px-10 py-5 mt-10">
-                <h1 className="text-3xl text-center sm:text-md font-bold text-left">Bonjour Monsieur/Madame</h1>
+                <h1 className="text-3xl text-center md:text-md font-bold text-left">Bonjour Monsieur/Madame</h1>
 
                 <div className="flex gap-4 justify-between md:justify-center items-center">
                     
